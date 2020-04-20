925e616e998fef92fbbb5e58848910c6c2a48c92
Bobby Brice <bobby.brice@gmail.com>
2020-04-20 15:21:50 -0400

    added getRandomNumber Fn

---
+++ script.js
@@ -0,0 +1,24 @@
+// Write a guessing game where the user has to guess a secret number.
+
+//After every guess the program tells the user whether their number was too large or too small.
+
+//At the end, the number of tries needed should be printed.
+
+// Inputting the same number multiple times should only count as one try.
+
+//If the user provides an answer which isn 't a number, print an error message and do not count this as a try.
+
+let prompt = require("prompt-sync")();
+let count = 0;
+
+// code below (replace this example)
+let answer = prompt("Guess a number between 1 and 20: ");
+
+
+
+console.log("You answered: " + answer);
+
+function getRandomNumber () {
+  return Math.floor(Math.random() * 20);
+}
+getRandomNumber();
